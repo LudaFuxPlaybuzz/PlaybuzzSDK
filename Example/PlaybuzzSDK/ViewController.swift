48f69a06998748d9fc9e0baa86a10b8687335283
Luda Fux <fuxlud@gmail.com>
2016-11-10 16:56:57 +0200

    removing old files

--- Example/PlaybuzzSDK/ViewController.swift
+++ Example/PlaybuzzSDK/ViewController.swift
@@ -9,21 +9,12 @@
 import UIKit
 import PlaybuzzSDK
 
-class ViewController: UIViewController, PlaybuzzWebViewProtocol, SettingsTableViewControllerProtocol{
+class ViewController: UIViewController, PlaybuzzWebViewProtocol{
     
     @IBOutlet weak var webViewConstraint: NSLayoutConstraint!
     @IBOutlet weak var containerHeight: NSLayoutConstraint!
     @IBOutlet weak var playbuzzView: PlaybuzzWebView!
     @IBOutlet weak var viewsContainer: UIView!
-    
-    static var showRecommendations = true
-    static var showShareButton = true
-    static var showFacebookComments = true
-    static var showItemInfo = true
-    
-    let itemAlias = "shpaltman/10-best-commercials-for-the-olympic-games-rio-2016"
-    let userID = UIDevice.current.identifierForVendor!.uuidString
-    let companyDomain = "http://www.playbuzz.com"
     
     let playbuzzQuiz = PlaybuzzQuiz(frame: CGRect(x: 8, y: 300, width: 400, height: 200))
     
@@ -38,14 +29,6 @@ class ViewController: UIViewController, PlaybuzzWebViewProtocol, SettingsTableVi
 //        viewsContainer.addSubview(playbuzzQuiz)
     }
     
-    override func prepare(for segue: UIStoryboardSegue, sender: Any!)
-    {
-        if let viewController = segue.destination as? SettingsTableViewController
-        {
-            viewController.delegate = self
-        }
-    }
-    
     //MARK: PlaybuzzWebView Protocol
     func resizePlaybuzzContainer(_ height: CGFloat){
         webViewConstraint.constant = height
@@ -54,20 +37,30 @@ class ViewController: UIViewController, PlaybuzzWebViewProtocol, SettingsTableVi
     
     func reloadItem()
     {
+        
+        let itemAlias = "shpaltman/10-best-commercials-for-the-olympic-games-rio-2016"
+        let userID = UIDevice.current.identifierForVendor!.uuidString
+        let companyDomain = "http://www.playbuzz.com"
+        
+        let showRecommendations = true
+        let showShareButton = true
+        let showFacebookComments = true
+        let showItemInfo = true
+        
         playbuzzQuiz.reloadItem(userID,
                                 itemAlias: itemAlias,
-                                showRecommendations: ViewController.showRecommendations,
-                                showShareButton: ViewController.showShareButton,
-                                showFacebookComments: ViewController.showFacebookComments,
-                                showItemInfo: ViewController.showItemInfo,
+                                showRecommendations: showRecommendations,
+                                showShareButton: showShareButton,
+                                showFacebookComments: showFacebookComments,
+                                showItemInfo: showItemInfo,
                                 companyDomain: companyDomain)
         
         playbuzzView.reloadItem(userID,
                                 itemAlias: itemAlias,
-                                showRecommendations: ViewController.showRecommendations,
-                                showShareButton: ViewController.showShareButton,
-                                showFacebookComments: ViewController.showFacebookComments,
-                                showItemInfo: ViewController.showItemInfo,
+                                showRecommendations: showRecommendations,
+                                showShareButton: showShareButton,
+                                showFacebookComments: showFacebookComments,
+                                showItemInfo: showItemInfo,
                                 companyDomain: companyDomain)
     }
 }

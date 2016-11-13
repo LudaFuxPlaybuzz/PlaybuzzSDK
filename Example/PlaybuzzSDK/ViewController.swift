e9f2ce4f61fdfd4f576be15246af4ba2cb3620c8
Luda Fux <fuxlud@gmail.com>
2016-11-13 15:49:21 +0200

    view from storyboard

--- Example/PlaybuzzSDK/ViewController.swift
+++ Example/PlaybuzzSDK/ViewController.swift
@@ -13,20 +13,17 @@ class ViewController: UIViewController, PlaybuzzQuizProtocol{
     
     @IBOutlet weak var containerHeight: NSLayoutConstraint!
     @IBOutlet weak var viewsContainer: UIView!
-    
-    let playbuzzQuiz = PlaybuzzQuiz(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height))
+    @IBOutlet weak var playbuzzQuiz: PlaybuzzQuiz!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.playbuzzQuiz.delegate = self
         self.reloadItem()
-        viewsContainer.addSubview(playbuzzQuiz)
     }
     
     func reloadItem()
     {
-        
         let itemAlias = "shpaltman/10-best-commercials-for-the-olympic-games-rio-2016"
         let userID = UIDevice.current.identifierForVendor!.uuidString
         let companyDomain = "http://www.playbuzz.com"

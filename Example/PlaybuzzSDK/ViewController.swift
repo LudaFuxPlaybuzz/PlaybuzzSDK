349754c797ee428ab7c08b27f4bfbc6b94e07ee7
Luda Fux <fuxlud@gmail.com>
2017-02-18 19:45:02 +0200

    alias and item id full cycle

--- Example/PlaybuzzSDK/ViewController.swift
+++ Example/PlaybuzzSDK/ViewController.swift
@@ -17,7 +17,6 @@ class ViewController: UIViewController{
 
         let embedCode = "<script type=\"text/javascript\" src=\"//cdn.playbuzz.com/widget/feed.js\"></script><div class=\"pb_feed\" data-embed-by=\"bcf88815-8496-421d-89ad-76793fdcd387\" data-item=\"7885f470-9bf5-468b-8eff-1c1ac7f7738a\" ></div>"
         
-//        let embedCode = "<script type=\"text/javascript\" src=\"//cdn.playbuzz.com/widget/feed.js\"></script><div class=\"pb_feed\" data-embed-by=\"bcf88815-8496-421d-89ad-76793fdcd387\" data-game=\"/matthewm14/how-fast-can-you-find-the-sleeping-bird\" ></div>"
         let companyDomain = "http://www.example.com"
         
         let playbuzzView = PlaybuzzView.init(frame: self.view.frame)
--- Example/Pods/PlaybuzzSDK/PlaybuzzSDK/Classes/PlaybuzzItemFetcher.swift
+++ Example/Pods/PlaybuzzSDK/PlaybuzzSDK/Classes/PlaybuzzItemFetcher.swift
@@ -16,48 +16,7 @@ class PlaybuzzItemFetcher: NSObject
         
         if let url = URL(string: "http://rest-api-v2.playbuzz.com/v2/items?id=\(itemID)")
         {
-            var request = URLRequest(url: url)
-            request.httpMethod = "GET"
-            
-            let task = URLSession.shared.dataTask(with: request) { data, response, error in
-                guard let data = data, error == nil else {
-                    print("PlaybuzzSDK, getItemData: error=\(error)")
-                    return
-                }
-                
-                if let httpStatus = response as? HTTPURLResponse
-                {
-                    if httpStatus.statusCode == 200
-                    {
-                        do {
-                            
-                            let parsedData = try JSONSerialization.jsonObject(with: data, options: []) as! [String:Any]
-                            if let payload = parsedData["payload"] as? [String:Any]
-                            {
-                                if let items = payload["items"] as? [Any]
-                                {
-                                    if items.count > 0
-                                    {
-                                        if let item = items[0] as? [String:Any]
-                                        {
-                                            let channelId = item["channelId"] as! String
-                                            
-                                            PlaybuzzAnalyticsManager.sendAnaliticsWithItemID(itemID, channelId: channelId, companyDomain: companyDomain)
-                                        }
-                                    }
-                                }
-                            }
-                        } catch let error as NSError {
-                            print(error)
-                        }
-                    }
-                    else
-                    {
-                        print("PlaybuzzSDK, getItemData: statusCode should be 200, but is \(httpStatus.statusCode). response = \(response)")
-                    }
-                }
-            }
-            task.resume()
+            PlaybuzzItemFetcher.parseItemDataAndSendAnalytics(url: url, companyDomain: companyDomain)
         }
     }
 
@@ -74,49 +33,55 @@ class PlaybuzzItemFetcher: NSObject
         
         if let url = URL(string: "http://rest-api-v2.playbuzz.com/v2/items?itemAlias=\(formatedAlias)")
         {
-            var request = URLRequest(url: url)
-            request.httpMethod = "GET"
+            PlaybuzzItemFetcher.parseItemDataAndSendAnalytics(url: url, companyDomain: companyDomain)
+        }
+    }
+    
+    class func parseItemDataAndSendAnalytics(url:URL,
+                                             companyDomain: String)
+    {
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let data = data, error == nil else {
+                print("PlaybuzzSDK, getItemData: error=\(error)")
+                return
+            }
             
-            let task = URLSession.shared.dataTask(with: request) { data, response, error in
-                guard let data = data, error == nil else {
-                    print("PlaybuzzSDK, getItemData: error=\(error)")
-                    return
-                }
-                
-                if let httpStatus = response as? HTTPURLResponse
+            if let httpStatus = response as? HTTPURLResponse
+            {
+                if httpStatus.statusCode == 200
                 {
-                    if httpStatus.statusCode == 200
-                    {
-                        do {
-                            
-                            let parsedData = try JSONSerialization.jsonObject(with: data, options: []) as! [String:Any]
-                            if let payload = parsedData["payload"] as? [String:Any]
+                    do {
+                        
+                        let parsedData = try JSONSerialization.jsonObject(with: data, options: []) as! [String:Any]
+                        if let payload = parsedData["payload"] as? [String:Any]
+                        {
+                            if let items = payload["items"] as? [Any]
                             {
-                                if let items = payload["items"] as? [Any]
+                                if items.count > 0
                                 {
-                                    if items.count > 0
+                                    if let item = items[0] as? [String:Any]
                                     {
-                                        if let item = items[0] as? [String:Any]
-                                        {
-                                            let itemID = item["id"] as! String
-                                            let channelId = item["channelId"] as! String
-                                            
-                                            PlaybuzzAnalyticsManager.sendAnaliticsWithItemID(itemID, channelId: channelId, companyDomain: companyDomain)
-                                        }
+                                        let itemID = item["id"] as! String
+                                        let channelId = item["channelId"] as! String
+                                        
+                                        PlaybuzzAnalyticsManager.sendAnaliticsWithItemID(itemID, channelId: channelId, companyDomain: companyDomain)
                                     }
                                 }
                             }
-                        } catch let error as NSError {
-                            print(error)
                         }
-                    }
-                    else
-                    {
-                        print("PlaybuzzSDK, getItemData: statusCode should be 200, but is \(httpStatus.statusCode). response = \(response)")
+                    } catch let error as NSError {
+                        print(error)
                     }
                 }
+                else
+                {
+                    print("PlaybuzzSDK, getItemData: statusCode should be 200, but is \(httpStatus.statusCode). response = \(response)")
+                }
             }
-            task.resume()
         }
+        task.resume()
     }
 }
